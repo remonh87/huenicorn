4c0bca4c907adaa63ba0470b4da4b18c5dc7e34a
miguelcmedeiros <miguel.catarino.medeiros@lighting.com>
2018-03-23 16:25:42 +0100

    removed unused import

--- lib/ui/IpAddress.dart
+++ lib/ui/IpAddress.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:huenicorn/BridgeStateProvider.dart';
 import 'package:huenicorn/Settings.dart';
 import 'package:huenicorn/main.dart';
 
