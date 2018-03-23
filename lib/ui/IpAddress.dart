9aeb20522d47034d31cafffdb139a8b17e10b8c9
miguelcmedeiros <miguel.catarino.medeiros@lighting.com>
2018-03-23 17:14:52 +0100

    improve screen

--- lib/ui/IpAddress.dart
+++ lib/ui/IpAddress.dart
@@ -21,6 +21,7 @@ class LoginPageState extends State<IpAddress>
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: new AppBar(
         backgroundColor: Colors.grey[850],
         leading: new IconButton(
@@ -33,89 +34,80 @@ class LoginPageState extends State<IpAddress>
             }
           },
         ),
-        title: new Text('IP Address'),
+        title: new Text('Connect to Bridge'),
       ),
-      body: new Stack(fit: StackFit.expand, children: <Widget>[
-
-        new Theme(
-          data: new ThemeData(
-              brightness: Brightness.light,
-              inputDecorationTheme: new InputDecorationTheme(
-                hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
-                labelStyle:
-                new TextStyle(color: Colors.black, fontSize: 25.0),
-              )),
-          isMaterialAppTheme: true,
-
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-
-              new Container(
-                padding: const EdgeInsets.all(30.0),
-                child: new Form(
-                  autovalidate: true,
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      new TextFormField(
-                          controller: _controller,
-                          decoration: new InputDecoration(
-                              labelText: "Please enter ip address",
-                              fillColor: Colors.black),
-                          keyboardType: TextInputType.number
+      body: new Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            new Theme(
+              data: new ThemeData(
+                  brightness: Brightness.light,
+                  inputDecorationTheme: new InputDecorationTheme(
+                    hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+                    labelStyle: new TextStyle(color: Colors.grey[600], fontSize: 25.0),
+                  )),
+              isMaterialAppTheme: true,
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Container(
+                    padding: const EdgeInsets.all(30.0),
+                    child: new Form(
+                      autovalidate: true,
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          new TextFormField(
+                            controller: _controller,
+                            decoration: new InputDecoration(
+                                labelText: "Please enter ip address"),
+                            keyboardType: TextInputType.number,
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 40.0),
+                          ),
+                          new MaterialButton(
+                            height: 50.0,
+                            minWidth: 150.0,
+                            color: Colors.green,
+                            splashColor: Colors.teal,
+                            textColor: Colors.white,
+                            child: new Icon(FontAwesomeIcons.check),
+                            onPressed: () {
+                              if (_controller.text.trim() == "") {
+                                showEnterIpDialog();
+                              } else {
+                                Settings.getInstance().setBridgeAddress(
+                                    _controller.text);
+                                Navigator.pop(context);
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) =>
+                                        new HuenicornHome()
+                                    )
+                                );
+                              }
+                            },
+                          )
+                        ],
                       ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 40.0),
-                      ),
-                      new MaterialButton(
-                        height: 50.0,
-                        minWidth: 150.0,
-                        color: Colors.green,
-                        splashColor: Colors.teal,
-                        textColor: Colors.white,
-                        child: new Icon(FontAwesomeIcons.signInAlt),
-                        onPressed: () {
-                          if (_controller.text.trim() == "") {
-                            showEnterIpDialog();
-                          } else {
-                            Settings.getInstance().setBridgeAddress(
-                                _controller.text);
-                            Navigator.pop(context);
-                            Navigator.push(
-                              context,
-                              new MaterialPageRoute(
-                                  builder: (context) =>
-                                    new HuenicornHome()
-                              )
-                            );
-                          }
-                        },
-
-                      )
-                    ],
-
+                    ),
                   ),
-                ),
+                  new Image(
+                    image: new AssetImage("assets/bridge_and_router.webp"),
+                    height: 200.0,
+                    fit: BoxFit.contain,
+                  ),
+                ],
               ),
-              new Image(
-                image: new AssetImage("assets/bridge_and_router.webp"),
-                height: 200.0,
-                fit: BoxFit.contain,
-                //colorBlendMode: BlendMode.darken,
-                //color: Colors.black87,
-              ),
-            ],
-          ),
-        ),
-      ]),
+            ),
+          ]),
     );
   }
 
   void showEnterIpDialog() {
-    showDialog(
-        context: context,
+    showDialog(context: context,
         child: new AlertDialog(
           title: new Text('Please type IP Address'),
         ));
