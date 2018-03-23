d626e1da2aef9c79bf3a35619e836bc924fbdb38
gaurav <600010522@MacBook-Pro-4.ddns.hightechcampus.nl>
2018-03-23 15:59:38 +0100

    updated color picker to dialog color picker

--- lib/LightColorPicker.dart
+++ lib/LightColorPicker.dart
@@ -1,8 +1,9 @@
 import 'package:color/color.dart' as NColor;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:huenicorn/BridgeState.dart';
 import 'package:huenicorn/hue/light.dart';
-import 'package:material_pickers/material_pickers.dart';
 
 
 class LightColorPicker extends StatefulWidget {
@@ -29,57 +30,39 @@ class LightPickerState extends State<LightColorPicker> {
 
   @override
   Widget build(BuildContext context) {
-    Color colorCode = new Color(121221);
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text("Color Picker"),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('$colorCode',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1,),
-            new ColorPicker(
-              type: MaterialType.transparency,
-              onColor: (color) {
-                NColor.HslColor ncolor = new NColor.Color.rgb(
-                    colorCode.red, colorCode.green, colorCode.blue)
-                    .toHslColor();
-                light.hue = ncolor.h;
-                light.brightness = 99.0;
-                bridgeClient.setLight(light);
-                setState(() {
-                  colorCode = color;
-                });
-              },
-              currentColor: colorCode,
-            )
-          ],
+    Color pickerColor = new Color(0xff443a49);
+    Color currentColor = new Color(0xff443a49);
+    ValueChanged<Color> onColorChanged;
+    return new AlertDialog(
+      title: const Text('Pick a color!'),
+      content: new SingleChildScrollView(
+        child: new ColorPicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+          enableLabel: true,
+          pickerAreaHeightPercent: 0.8,
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      actions: <Widget>[
+        new FlatButton(
+          child: new Text('Got it'),
+          onPressed: () {
+            setState(() => currentColor = pickerColor);
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
     );
+  }
+
+  // bind some values with [ValueChanged<Color>] callback
+  changeColor(Color color) {
+    NColor.HslColor ncolor = new NColor.Color.rgb(
+        color.red, color.green, color.blue)
+        .toHslColor();
+    light.hue = ncolor.h;
+    light.brightness = 0.99;
+    bridgeClient.setLight(light);
   }
 
 }
